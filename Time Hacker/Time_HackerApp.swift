2adbd7461e2c4181b0be5fa68d310ab4ca08108a
bakstag147 <milakowladimir@gmail.com>
2024-11-05 09:39:44 +0100

    222

---
+++ Time Hacker/Time_HackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_HackerApp.swift
+//  Time Hacker
+//
+//  Created by Vladimir Milakov on 05.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Time_HackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
